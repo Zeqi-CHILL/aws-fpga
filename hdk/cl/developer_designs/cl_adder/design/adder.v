23a61f38602b6c0c45297e3cdfda344d774a2ac5
Zeqi-CHILL <zeqiqin@umass.edu>
2018-12-05 21:01:16 -0500

    wrap cl_adder&cl_fifo_selfadd

---
+++ hdk/cl/developer_designs/cl_adder/design/adder.v
@@ -0,0 +1,21 @@
+module adder(clock,reset,add_in1,add_in2,add_out);
+
+input wire clock;
+input wire reset;
+input wire [3:0] add_in1;
+input wire [3:0] add_in2;
+output reg [4:0] add_out;
+
+always @(posedge clock)
+	begin
+		add_out <= add_in1 + add_in2;
+	end
+
+always @(posedge clock)
+	begin
+	if(!reset)
+		begin
+			add_out <=5'b00000;
+		end
+	end
+endmodule
